3ee5c7823c7d47c84cfd8a58e6189bbb846853b2
sumit sharma <sumitsharmavoice@gmail.com>
2026-04-02 02:43:47 +0530

    cors issue fixeds

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://taskmanagerfrontend-eta.vercel.app/",
+    origin: "https://taskmanagerfrontend-eta.vercel.app",
     credentials: true
   })
 );
