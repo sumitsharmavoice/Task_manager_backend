dff4e95a78516eddc0dc2de94220445eafd6706b
sumit sharma <sumitsharmavoice@gmail.com>
2026-04-02 00:36:33 +0530

    fixed prisma errors

--- dist/controllers/task.controller.js
+++ dist/controllers/task.controller.js
@@ -10,7 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getTasks = exports.createTask = void 0;
-const client_1 = require("../generated/prisma/client");
+const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 const createTask = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { title, description } = req.body;
--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const createTask = async (req: any, res: any) => {
